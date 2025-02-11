fc3040fea9617dba867be228ea44140d4b68b712
DevSalot493 <devsalot@gmail.com>
2025-02-12 00:12:02 +0530

    Create homepage.js

---
+++ homepage.js
@@ -0,0 +1,14 @@
+function tsez()
+{
+	localStorage.setItem('ts', '100');
+}
+
+function tsmid()
+{
+	localStorage.setItem('ts', '50');
+}
+
+function tshard()
+{
+	localStorage.setItem('ts', '25');
+}
